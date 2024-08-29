2c131552da1f3e207ac6e64f71a818b319ca943c
unknown <niccau1203@gmail.com>
2024-08-29 11:25:50 -0300

    feat: add create emotion request

--- src/server.js
+++ src/server.js
@@ -8,6 +8,24 @@ const serverPort = process.env.PORT || 3000
 const app = express()
 app.use(express.json())
 
+const emocoes = [
+    {
+        id: 1,
+        nome: "Nojinho",
+        cor: "Verde"
+    },
+    {
+        id: 2,
+        nome: "Raiva",
+        cor: "Vermelho"
+    },
+    {
+        id: 3,
+        nome: "Alegria",
+        cor: "Amarelo"
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
 })
@@ -16,6 +34,48 @@ app.get("/2tds2", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
 })
 
+app.get("/emocoes", (req, res) => {
+    return res.status(200).send(emocoes)
+})
+
+const personagens = [
+    {
+        id: 100,
+        nome: "Totoro",
+        studio: "Ghibil",
+        vivo: true
+    },
+
+    {
+        id: 101,
+        nome: "Rapunzel",
+        studio: "Disney",
+        vivo: true
+    },
+
+    {
+        id: 102,
+        nome: "Bela e a fera",
+        studio: "Disney",
+        vivo: true
+    }
+]
+
+app.get("/personagens", (req, res) => {
+    return res.status(200).send( personagens )
+})
+
+app.post("/emocoes", (req, res) => {
+    const { nome, cor } = req.body
+    const novaEmocao = {
+        id: emocoes.length + 1,
+        nome: nome,
+        cor: cor
+    }
+    emocoes.push(novaEmocao)
+    return res.status(200).send( emocoes )
+})
+
 app.listen(serverPort, () => {
     console.log(`💖 Server started on http://localhost:${serverPort}`)
-})
+});
